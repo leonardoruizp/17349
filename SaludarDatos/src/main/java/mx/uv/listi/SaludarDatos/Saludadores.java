440df5f805ad89108a9a04c707a15530dcc431a4
leonardoruizp <leorp3101@gmail.com>
2025-12-02 16:49:37 -0600

    modificacion

--- SaludarDatos/src/main/java/mx/uv/listi/SaludarDatos/Saludadores.java
+++ SaludarDatos/src/main/java/mx/uv/listi/SaludarDatos/Saludadores.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Saludadores {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
 
